b1f9aabb47b788f9fa76c390fb86d19102f498a1
Nick Roberts <nickroberts217@gmail.com>
2023-12-07 15:01:25 -0500

    chore: update blurbs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
-<section class="hero is-link">
-	<div class="hero-body">
-		<h1 class="title is-1">Nick Roberts</h1>
-		<h6 class="subtitle is-6">Backend Engineer</h6>
+<section class="hero">
+	<div class="hero-body pb-0">
+		<h1 class="title is-1">Nicholas Roberts</h1>
+		<h6 class="subtitle is-6">Backend Engineer | Django Developer</h6>
   	</div>
 </section>
 <section class="hero">
@@ -13,7 +13,7 @@
 				<p>Freelancing</p>
 			</div>
 			<div class="column">
-				<p>Part-time, full stack development on TheArcherApp, an application that helps manage over 30 senior living facilities in the Midwest. Current projects include employee scheduling, PDF generation and a Twilio integration.</p>
+				<p>Full stack development of a Django web application that manages over 30 senior living facilities in the Midwest. Completed projects include integrating Twilio SMS notifications and a significant rewrite of the frontend to a modern JS framework.</p>
 			</div>
 		</div>
 		<div class="columns is-mobile">
@@ -21,7 +21,7 @@
 				<a href="https://www.joinforma.com" target="_blank">Forma</a>
 			</div>
 			<div class="column">
-				<p>One of three backend engineers on the Member Experience team. Proudest accomplishment is building a microservice that enabled users to spend their HSA balance on the NYSE. Used message queues and publishers/subscribers.</p>
+				<p>One of five engineers on the Core Product team. Key accomplishment is architecting, building, and deploying a Node.js microservice that served as the API of the HSA investments feature. Largely event-driven by message queues and GCP publishers/subscribers.</p>
 			</div>
 		</div>
 		<div class="columns is-mobile">
@@ -29,7 +29,7 @@
 				<a href="https://www.teampay.co" target="_blank">Teampay</a>
 			</div>
 			<div class="column">
-				<p>One of the early hires. API development and integrations. Proudest accomplishment is the addition of purchase orders, which attracted bigger mid-market customers that needed to make sizeable purchases.</p>
+				<p>One of the early hires. API development and integrations. Key accomplishment is the addition of purchase orders as a payment option, which attracted larger mid-market customers that needed to track sizeable purchases.</p>
 			</div>
 		</div>
 		<br>
@@ -40,7 +40,7 @@
 				<a href="https://www.roux.coffee" target="_blank">Roux</a>
 			</div>
 			<div class="column">
-				<p>Utilizes crowdsourcing and Google's Places API to verify amenities offered in food and drink establishments. Started with coffee shops in NYC.</p>
+				<p>Utilizes crowdsourcing and Google's Places API to verify amenities offered in food and drink establishments. Started with coffee shops in NYC. Down currently due to the hefty cost of GCP but the code is available <a href="https://github.com/nickroberts217" target="_blank">here</a>.</p>
 			</div>
 		</div>
 		<br>
@@ -51,7 +51,7 @@
 				<p>Backend</p>
 			</div>
 			<div class="column">
-				<p>Python, Django and PostgreSQL. A few years with MERN. Experience with both GCP and AWS.</p>
+				<p>Python, Django, and PostgreSQL. A few years with MERN. Experience with both GCP and AWS.</p>
 			</div>
 		</div>
 		<div class="columns is-mobile">
@@ -59,13 +59,13 @@
 				<p>Web</p>
 			</div>
 			<div class="column">
-				<p>HTML, CSS and Django. Recently some Alpine.js and Svelte.js.</p>
+				<p>HTML, CSS, and Django. Recently some Alpine.js and Svelte.js.</p>
 			</div>
 		</div>
 	</div>
 </section>
 <section class="hero">
-	<div class="hero-body">
+	<div class="hero-body mt-4">
 		<p class="is-size-7">&copy; 2023</p>
 	</div>
 </section>
